b60733566bf7dcb6cd1d37ca60613080761aeae6
NajlaBH-Angular <BHNajla.bioinfo@gmail.com>
2021-11-23 16:35:13 +0000

    :wrench: Add the button ADD.

--- src/app/cmainbox/cmainbox.component.ts
+++ src/app/cmainbox/cmainbox.component.ts
@@ -9,30 +9,48 @@ import { Component, OnInit } from '@angular/core';
 export class CmainboxComponent implements OnInit {
 
   //Declaration
+  
   //Get the input task
   inputTask:string="";
-  taskList:string[]=[];
+  //Get the task list
+  taskList:string[]=['No tasks yet :)'];
+  //Get the task index
+  //idx:number=1;
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
+  //Delete no tasks yet ...
   spliceIt(element: string,stringArray:string[]){
     stringArray.forEach((value,index)=>{
         if(value==element) stringArray.splice(index,1);
     });
   }
+
+
+  //Add task to the list
   addToTaskList(){
     //console.log('it does nothing',this.inputTask);
     if(this.inputTask != "Please add your task here." && this.inputTask != ""){
 	    this.spliceIt('No tasks yet :)',this.taskList);
 	    this.taskList.push(this.inputTask);
+    }else if (this.inputTask == ""){
+	    alert("Please add your task in the empty field.")
     }else{
 	    if(this.taskList=[]){
 		    this.taskList=['No tasks yet :)'];
 	    }
     }
   }
+
+  //Delete task
+  //deleteTask(index:number) {
+  //      this.taskList.splice(index, 1);
+  //  }
 
 }
